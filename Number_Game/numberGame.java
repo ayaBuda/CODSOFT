e4ec289c3a45f49990239bda047d607a99fb2514
unknown <ayandabuda73@gmail.com>
2024-01-11 16:45:48 +0200

    added the basics of the game. keeps track of lives, gets input and ends when one loses

---
+++ Number_Game/numberGame.java
@@ -0,0 +1,57 @@
+import  java.util.Random;
+import java.util.Scanner;
+
+public class numberGame{
+    public static void main(String[] args){
+        Random random = new Random();
+
+        int minRange = 1;
+        int maxRange = 100;
+
+        int generatedNumber = random.nextInt(maxRange - minRange+1) + maxRange;
+
+        int noOfLives = 5;
+
+        Scanner scanner =  new Scanner(System.in);
+        System.out.println("WELCOME TO GUESS THE NUMBER!");
+        System.out.println(generatedNumber);
+        System.out.println("Number of lives: "+ noOfLives);
+
+
+        while(noOfLives > 0){
+            System.out.println("Please guess the number: ");
+            int usersGuess = scanner.nextInt();
+
+            if (generatedNumber == usersGuess){
+                System.out.println("CONGRADULATIONS! You've Guessed It Correct!");
+                break;
+            }
+            else {
+                if(noOfLives == 0){
+                   System.out.println("You Lose. The number was: "+generatedNumber); 
+                }
+                // if(generatedNumber != usersGuess){
+                else if (usersGuess > generatedNumber){
+                    System.out.println("Too High. Try again!");
+                    noOfLives --;
+                    System.out.println("Lives left: "+ noOfLives);
+                }
+                else if (usersGuess < generatedNumber){
+                    System.out.println("Too Low. Try again!");
+                    noOfLives --;
+                    System.out.println("Lives left: "+ noOfLives);
+                }
+               
+                
+            }
+            // }
+            
+           
+
+
+        }
+
+
+    }
+
+}
